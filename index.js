fef617ab8c5781592a4b1f54c081280b60476f03
Caleb Durden <calebdurden@gmail.com>
2022-05-18 05:37:25 -0400

    commit tree 18d7d62c158f980590ef18b4069d7033fce931b8

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+    const Hq = 42;
+    if (pickup < Hq) {
+        return (Hq - pickup)
+    } else if (pickup > Hq) {
+        return (pickup - Hq)
+    }
+}
+
+function distanceFromHqInFeet(location) {
+    const blocks = distanceFromHqInBlocks(location);
+    return (blocks * 264)
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start < destination) {
+        return (destination - start) * 264
+    } else if (start > destination) {
+        return (start - destination) * 264
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+   const feet = distanceTravelledInFeet(start, destination)
+   let fare;
+   if (feet <= 400) {
+       return 0
+   } else if (feet <= 2000) {
+       fare = (feet - 400) * .02;
+       return fare;
+   } else if (feet <= 2500) {
+       fare = 25;
+       return fare;
+   } else if (feet > 2500) {
+       return "cannot travel that far"
+   }
+}
